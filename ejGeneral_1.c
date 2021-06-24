53ac5e35a842dbd4319821079e63939e6ba43b14
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-06-24 10:16:49 -0300

    Add files via upload

---
+++ ejGeneral_1.c
@@ -0,0 +1,17 @@
+/*
+	
+	Simular un piedra papel o tijera
+
+	Aspectos a tener en cuenta:
+		- Simular el jugador 2 con un "bot", este bot debe poder aleatoriamente elegir piedra papel o tijera
+		- El jugador 1 es el usuario
+		- Gana el que elija la mejor opcion, si hay empate comienza otra ronda
+		
+	Requisitos:
+		- Generar un  numero aleatorio para la eleccion del bot
+		- Desplegar un menu para el usuario correspondiente al juego
+		- El programa termina cunado gana algun jugador
+		- Informar la situacion al finalizar el programa
+	
+	EXTRA 1: implementar al mejor de 3 
+*/
---
+++ ejGeneral_2.c
@@ -0,0 +1,28 @@
+/*	
+	
+	Simular un juego de azar donde hay un numero ganador y el usuario podra intentar ganar
+	un premio
+
+	Aspectos a tener en cuenta:
+		- el numero ganador es 8
+		- fichas hasta 10 con posibilidad de volver a comprar
+		- costo de las fichas es de 149$
+		- el usuario cuenta con un saldo inicial
+		- premio es igual a la cantidad de fichas usadas multiplicado por el numero ganador y 1000
+		- incialmente el usuario cuenta con una ficha de regalo
+		- el rango de numeros aleatorios es de 0 a 20
+
+	Requisitos:
+		-Solicitar informacion inicial para el funcionamiento del programa
+		-Desplegar un menu informando las opciones:
+			-Probar suerte
+			-Comprar fichas
+			-Salir
+		-El programa debe poder generar un numero aleatorio para simular la suerte
+		-Si el usuario gana o sale se debe informar la situacion como corresponda
+	
+	rand y srand info: http://www.chuidiang.org/clinux/funciones/rand.php
+
+	EXTRA 1: Agregar opcion de aumentar saldo
+	EXTRA 2: agregar oferta donde comprando 5 fichas se  realiza un descuento del 15%
+*/
