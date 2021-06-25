b5236112eabd23c31dd44d249d58b27889f63071
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-06-25 11:02:45 -0300

    Add files via upload

---
+++ ejGeneral_3.c
@@ -0,0 +1,26 @@
+/*
+	
+	Simulando un termo 
+
+	Crear un programa que permita desplegar una simulacion del estado actual
+	del contenido en un termo visualmente
+
+	Aspectos a tener en cuenta:
+		- El termo tiene una capacidad de 1 litro
+		- Cada uso consume  un 10% del contenido
+		- Cada carga aumenta en un 10% del contenido
+
+	Requisitos:
+		- Representar un termo visualmente, a su criterio, con el estado del contenido 
+		- Menu que permita:
+			- usar termo
+			- cargar termo
+			- terminar simulacion
+		- Realizar las acciones necesarias bajo las situaciones donde
+			- El contenido llegue a su maximo
+			- No hay contenido
+
+	Extra: informar en cada uso el porcentaje del contenido
+	Extra: permitir al usuario poder variar la cantidad de carga del contenido asi como el uso	
+
+*/
