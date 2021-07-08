a0150215a55fab127c962f3d755b3f6f813620fe
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-07-08 13:07:19 -0300

    Add files via upload

--- ejGeneral_3.c
+++ ejGeneral_3.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*
 	
 	Simulando un termo 
@@ -24,3 +26,74 @@
 	Extra: permitir al usuario poder variar la cantidad de carga del contenido asi como el uso	
 
 */
+
+#define OPC_USAR 1
+#define OPC_AUMENTAR 2
+#define OPC_SALIR 3
+
+const int CONSUMO_PORCENTAJE = 10;
+const int CARGA_PORCENTAJE = 10;
+
+const int CAPACIDAD_PORCENTAJE = 100;
+const int TERMO_VACIO_PORCENTAJE = 0;
+
+const int PORCENTAJE_FRANJA = 10;
+
+#define TERMO_TAPA_1  "______"
+#define TERMO_TAPA_2 "|......|"
+#define TERMO_TAPA_3 "|^^^^^^|"
+
+#define TERMO_CONT_VACIO "      "
+#define TERMO_INDIC_CONT "______"
+
+#define TERMO_BORDE_IZQ "|"
+#define TERMO_BORDE_DER "|"
+
+#define TERMPO_BASE "|~~~~~~|"
+
+int main(){
+
+	int cantTermo = 70;
+	int opc = 0;
+
+	do{
+		printf("Termpo a un %i porciento\n", cantTermo);
+		printf("  %s\n", TERMO_TAPA_1);
+		printf(" %s\n", TERMO_TAPA_2);
+		printf(" %s\n", TERMO_TAPA_3);
+		for(int i = 0 ; i < CAPACIDAD_PORCENTAJE/PORCENTAJE_FRANJA ; i++){
+			printf(" %s", TERMO_BORDE_IZQ);
+			if(i < ( CAPACIDAD_PORCENTAJE -cantTermo)/PORCENTAJE_FRANJA){
+				printf(TERMO_CONT_VACIO);
+			}else{
+				printf(TERMO_INDIC_CONT);
+			}
+			printf("%s\n", TERMO_BORDE_DER);
+		}
+		printf(" %s\n", TERMPO_BASE);
+		
+		printf("Usar 1\n");
+		printf("Aumentar 2\n");
+		printf("Salir 3\n");
+		scanf("%i", &opc);
+
+		switch(opc){
+			case OPC_USAR:
+				if(cantTermo > TERMO_VACIO_PORCENTAJE){
+					cantTermo-=CONSUMO_PORCENTAJE;
+				}else{
+					printf("Te quedaste sin agua!\n");
+				}
+				break;
+			case OPC_AUMENTAR:
+				if(cantTermo<CAPACIDAD_PORCENTAJE){
+					cantTermo+=CARGA_PORCENTAJE;
+				}else{
+					printf("Maximo alcanzado cuidado!\n");
+				}
+				break;
+		}
+	}while(opc != OPC_SALIR);
+
+	return 0;
+}
